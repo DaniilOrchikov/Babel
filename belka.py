aec90186e571003f06fdb6c12317b172f07db0e0
danil <danilaor2009@gmail.com>
2022-03-19 21:17:30 +0300

    0.0.1.1 Не тот код запушил)

--- belka.py
+++ belka.py
@@ -1,24 +1,51 @@
-n = int(input())
+def create_alf():
+    alf = []
+    for r in prealf:
+        for g in prealf:
+            for b in prealf:
+                alf.append(r + g + b)
+    return alf
+
+
+def lcg():
+    a = 474748
+    c = l
+    m = 32768 ** 196608
+    global seed
+    seed = (a * seed + c) % m
+    return seed
+
+
+prealf = '0123456789abcdefghijklmnopqrstuv'
+
+alf = create_alf()
+lalf = len(alf)
+
+width = 72
+height = 128
+
+l = (len(prealf) ** 3 * 2) ** (width * height)
+
+room = '19832835253283383535385258533323232532'
+rack = '17877887877878786675645332456768654323454677889876544678987867543456778765435678987654678'
+shelf = '1432342343443232232423324'
+book = '1432432343443342344'
+page = '143242334243432243'
+
+seed = int(room + rack + shelf + book + page)
+
+num = lcg()
+b = []
+while num:
+    b.append(alf[num % lalf])
+    num //= lalf
+b = b[:9216]
+b.reverse()
+v = 0
 m = []
-for i in range(n):
-    b, x1, y1, x2, y2 = input().split()
-    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    m.append((b, x1, y1, x2, y2))
-aa = []
-for i in m:
-    v = 0
-    for j in m:
-        if j[0] != i[0]:
-            xmax1, xmin1, xmax2, xmin2, ymax1, ymin1, ymax2, ymin2 = \
-                max(i[1], i[3]), min(i[1], i[3]), max(j[1], j[3]), min(j[1], j[3]), \
-                max(i[2], i[4]), min(i[2], i[4]), max(j[2], j[4]), min(j[2], j[4])
-            if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
-                p1p3, p1p2, p1p4, p3p1, p3p4, p3p2 = (j[1] - i[1], j[2] - i[2]), (i[3] - i[1], i[4] - i[2]), \
-                                                     (j[3] - i[1], j[4] - i[2]), (i[1] - j[1], i[2] - j[2]), \
-                                                     (j[3] - j[1], j[4] - j[2]), (i[3] - j[1], i[4] - j[2])
-                if (p1p3[0] * p1p2[1] - p1p2[0] * p1p3[1]) * (p1p4[0] * p1p2[1] - p1p4[1] * p1p2[0]) <= 0 and \
-                        (p3p1[0] * p3p4[1] - p3p4[0] * p3p1[1]) * (p3p2[0] * p3p4[1] - p3p2[1] * p3p4[0]) <= 0:
-                    v += 1
-    aa.append((i, v))
-aa.sort(key=lambda x: x[1], reverse=True)
-print(*[i for i in aa[0][0][1:]])
+for i in range(height):
+    m.append([])
+    for j in range(width):
+        m[-1].append(b[v])
+        v += 1
+print(*m, sep='\n')
