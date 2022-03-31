ce7a0fb6a2cd825ce44f9cb2385a624597664c2e
Olya K <olya_komarova_0303@mail.ru>
2022-03-31 16:55:10 +0300

    change style.css, add index.html

--- useful_code/html_attributes.py
+++ useful_code/html_attributes.py
@@ -3,30 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def new_page():
-    """# создает отдельную вкладку с сайтом библиотеки"""
-    return """<a href="https://libraryofbabel.info/" target="_blank">Вавилонская библиотека</a>"""
-
-@app.route('/greeting/<username>')
-def greeting(username):
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                   <link rel="stylesheet"
-                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                   crossorigin="anonymous">
-                    <title>Привет, {username}</title>
-                  </head>
-                  <body>
-                    <h1>Привет, {username}!</h1>
-                  </body>
-                </html>'''
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+#@app.route('/')
+# def new_page():
+#     """# создает отдельную вкладку с сайтом библиотеки"""
+#     return """<a href="https://libraryofbabel.info/" target="_blank">Вавилонская библиотека</a>"""
+#
+# @app.route('/greeting/<username>')
+# def greeting(username):
+#     return f'''<!doctype html>
+#                 <html lang="en">
+#                   <head>
+#                     <meta charset="utf-8">
+#                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+#                    <link rel="stylesheet"
+#                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+#                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+#                    crossorigin="anonymous">
+#                     <title>Привет, {username}</title>
+#                   </head>
+#                   <body>
+#                     <h1>Привет, {username}!</h1>
+#                   </body>
+#                 </html>'''
+# if __name__ == '__main__':
+#     app.run(port=8080, host='127.0.0.1')
 
 
 # <!DOCTYPE html>
