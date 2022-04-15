181c381898a02042fe891ff7f191f9d14aed5e9a
Olya K <olya_komarova_0303@mail.ru>
2022-04-15 19:58:49 +0300

    1. finished sign in/up (html & css) 2. added pictures 3. refactoring

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ from data.users import User
 from flask_restful import Api
 from flask import render_template
 from forms.user import RegisterForm
+from flask_login import LoginManager
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from forms.user import LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
@@ -18,6 +22,16 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/data_base.db")
     app.run(port=8080, host='127.0.0.1')
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
@@ -65,16 +79,30 @@ def sign_up():  # регистрация
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        return redirect('/sign_in')
     return render_template('sign_up.html', title='Регистрация', form=form)
 
 
 @app.route('/sign_in', methods=['GET', 'POST'])
-def sing_in():
-    """страница с авторизацией"""
-    # if form.validate_on_submit():
-    #     return redirect('/account')
-    # return render_template('sign_in.html', title='Авторизация', form=form)
+def sign_in():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('sign_in.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('sign_in.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/image/<string:address>')
--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -9,4 +9,10 @@ class RegisterForm(FlaskForm):
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
     surname = StringField('Фамилия пользователя', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
